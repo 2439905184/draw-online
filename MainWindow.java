061dc93be88b140c48c33823423718435c8ec82b
2439905184 <29478722+2439905184@users.noreply.github.com>
2019-01-02 11:58:14 +0800

    Added MainWindow and setted the title !
    Added MainWindow and setted the title !

--- MainWindow.java
+++ MainWindow.java
@@ -1,4 +1,6 @@
 package draw_online;
+import java.awt.Button;
+import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Panel;
 import java.awt.Graphics;
@@ -10,19 +12,25 @@ public class MainWindow
 	
 	public static void main(String[] args) 
 	{
-		Frame f = new Frame("123");//新建框架并设置标题
+		Frame f = new Frame("Draw_Online");//新建框架并设置标题
 		Panel pl = new Panel();
+		Button button = new Button();
 		JLabel lab = new JLabel("hello world");//设置标签
-		f.setSize(300, 300);//设置框架大小
+		f.setSize(1320, 850);//设置框架大小
 		f.setVisible(true);//设置可见性
+		
+		button.setName("mybutton");
+		button.setSize(20, 200);
+		pl.setBackground(Color.gray);
 		f.add(pl);
 		pl.add(lab);
+		pl.add(button);
 		
-		MainWindow.drawline();
+		Graphics pen = pl.getGraphics();
+		pen.setColor(Color.RED);
+		pen.drawLine(0, 0, 0, 200);
+		
 		f.addWindowListener(new WindowsEvents());
 	}
-	public static void drawline()
-	{
-		
-	}
+	
 }
